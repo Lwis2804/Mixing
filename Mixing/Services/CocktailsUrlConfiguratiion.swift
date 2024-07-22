1be6f950a09f9ad27950b8d314f1a20fec0a8b2a
LUIS <luis.enrique.enc@gmail.com>
2024-07-22 17:09:56 -0600

    [ADD] configuration de url

--- Mixing/Services/CocktailsUrlConfiguratiion.swift
+++ Mixing/Services/CocktailsUrlConfiguratiion.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct CocktailsUrlCnfiguration {
+    
+    var metodo : String
+    var host : String
+    var path : PathsProtocol
+    
+    public init(metodo: String, host: String, path : PathsProtocol) {
+        self.metodo = metodo
+        self.host = host
+        self.path = path
+    }
+    
+    
+    func cofingUrl() -> URL? {
+        guard !host.isEmpty else { return nil }
+        var componentes : URLComponents = URLComponents()
+        componentes.scheme = metodo
+        componentes.host = host
+        componentes.path = path.pathToUse
+        return componentes.url
+    }
+}
