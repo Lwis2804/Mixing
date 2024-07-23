07dbfa57fac26d1c9fc15f9666d20581ba6b06cd
LUIS <luis.enrique.enc@gmail.com>
2024-07-22 21:00:54 -0600

    [ADD] se agrega collection views

---
+++ Mixing/Modules/CocktailsList/View/CocktailsListViewControllerExtension.swift
@@ -0,0 +1,20 @@
+//
+//  CocktailsListViewControllerExtension.swift
+//  Mixing
+//
+//  Created by LUIS GONZALEZ on 22/07/24.
+//
+
+import UIKit
+
+extension CocktailsListViewController : UICollectionViewDelegate & UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
--- Mixing/Views/CocktailsListCollectionViewCell.swift
+++ Mixing/Views/CocktailsListCollectionViewCell.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class CocktailsListCollectionViewCell: UICollectionViewCell {
-
+    @IBOutlet var cocktailImage: UIImageView!
+    @IBOutlet var cocktailName: UILabel!
+    
+    var downloadTask : URLSessionDownloadTask?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func configCocktailCell(withEntry entry : Drinks?) {
+        guard let entrada = entry else { return }
+        self.cocktailName.text = "\(entrada.strDrink ?? "")"
+        if let urlThumb = entrada.strDrinkThumb,
+           let url = URL(string:"https://www.thecocktaildb.com/images/media/drink/\(urlThumb)"){
+            downloadTask = self.cocktailImage.loadImage(url: url)
+        }
     }
 
 }
